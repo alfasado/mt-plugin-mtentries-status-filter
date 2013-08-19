3df857f108b703fcfc9e67ce8f6a8fc578c3d372
Alfasado Inc <webmaster@alfasado.jp>
2013-08-19 13:34:05 +0900

    Fixed. typo.

--- plugins/MTEntriesStatsFilter/lib/MTEntriesStatsFilter/L10N.pm
+++
@@ -1,5 +0,0 @@
-package MTEntriesStatsFilter::L10N;
-use strict;
-use base 'MT::Plugin::L10N';
-
-1;
--- plugins/MTEntriesStatsFilter/lib/MTEntriesStatusFilter/L10N.pm
+++
@@ -1,5 +0,0 @@
-package MTEntriesStatusFilter::L10N;
-use strict;
-use base 'MT::Plugin::L10N';
-
-1;
